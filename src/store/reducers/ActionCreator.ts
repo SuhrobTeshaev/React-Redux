fafef08742a69c1d5e745cd74ec6a31354145c6b
SuhrobTeshaev <125867321+SuhrobTeshaev@users.noreply.github.com>
2023-10-10 23:14:09 +0700

    n

--- src/store/reducers/ActionCreator.ts
+++ src/store/reducers/ActionCreator.ts
@@ -10,6 +10,5 @@ export const fetchUsers =   (dispatch:AppDispatch) => {
         dispatch(userSlice.actions.userFetchingSuccess(response.data))
     }catch(e) {
         dispatch(userSlice.actions.userFetchingError(e.message))
-        
     }
 }
